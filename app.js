a57eaf1fd522d165f9f598d59cf6232ebac7a300
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-05-10 11:35:12 -0500

    feat: Setup initial project structure and ES Module support
    - Updates .gitignore to ignore node_modules and .env.
    - Adds the main application entry point (app.js).
    - Updates package.json to set "type": "module" for ES Module support.

---
+++ app.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at http://localhost:${process.env.PORT}`);
+})
