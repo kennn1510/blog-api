f321509bd33c90e8bc0141ccb233a62711e74b5c
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-07-12 20:25:40 -0500

    Extra spacing and post example route

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,10 @@ import { Router } from "express";
 const router = new Router();
 
 router.get("/", (req, res) => {
-  res.send("Welcome");
+  res.send("Welcome to get");
+});
+router.post("/", (req, res) => {
+  res.send("Welcome to post");
 });
 
 export default router;
